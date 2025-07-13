38a38d7092337802c7ba61eacd32187dbafafc13
anurag8824 <suryapsnkr@gmail.com>
2025-07-13 19:11:56 +0530

    first commit

---
+++ src/config/connectDB.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql2/promise');
+
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'demoprojects',
+//     // user:'root',
+//     // password:'',
+//     // database:'goa',
+//     password: '#demoProjects#$4@',
+//     database: 'goagamesclub',
+//     port: 8000
+// });
+
+
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',  // XAMPP's default root password is blank
+//     database: 'goagamesclub',
+//     port: 3306
+// });
+
+
+const connection = mysql.createPool({
+        host: '127.0.0.1',
+        user: 'myuser',
+        password: 'mypassword',
+        database: 'myapp_db',
+        port: 3306
+    });
+    
+
+module.exports = connection;
